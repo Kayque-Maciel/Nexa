7cddabdce15214b948a383482f9fb9e35d1ab518
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-05-06 12:53:32 -0300

    Adicionando conteúdo na section

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,10 @@
 import style from "./css/Main.module.css"
 import mulher from "./images/mulher.png"
+import dinheiro from "./images/dinheiro.png"
+import mao from "./images/mao.png"
+import cofrinho from "./images/cofrinho.png"
+import pagamento from "./images/pagamento.png"
+import acoes from "./images/acoes.png"
 
 function Main(){
     return(
@@ -9,6 +14,16 @@ function Main(){
                 
             </div>
             <img src={mulher} alt="" className={style.mulher}/>
+            <div className={style.borda}>
+                <div className={style.icones}>
+                    <img src={dinheiro} alt="" />
+                    <img src={mao} alt="" />
+                    <img src={cofrinho} alt="" />
+                    <img src={pagamento} alt="" />
+                    <img src={acoes} alt="" />
+                </div>
+                
+            </div>
         </section>
         </>
     )
